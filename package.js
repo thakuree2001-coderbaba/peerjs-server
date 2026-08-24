fb2343bc9094007f2684b3155b7af0f5207633c1
thakuree2001-coderbaba <thakuree2001@gmail.com>
2026-08-24 20:24:13 +0400

    Initialize package.json for peerjs-server project

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "peerjs-server",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "peerjs --port 10000 --path /myapp"
+  },
+  "dependencies": {
+    "peer": "^1.0.2"
+  }
+}
